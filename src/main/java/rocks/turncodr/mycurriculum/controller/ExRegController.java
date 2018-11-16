1c547944ee4121dacc17b73db7e932775e06992b
Bastian Schattenberg <git@bestyan.de>
2018-11-16 14:07:07 +0100

    fixed moduleCreate to at least work

--- src/main/java/rocks/turncodr/mycurriculum/controller/ExRegController.java
+++ src/main/java/rocks/turncodr/mycurriculum/controller/ExRegController.java
@@ -7,12 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import rocks.turncodr.mycurriculum.model.ExReg;
-import rocks.turncodr.mycurriculum.model.ExRegWrapper;
-import rocks.turncodr.mycurriculum.model.Module;
 import rocks.turncodr.mycurriculum.services.ExRegJpaRepository;
 import rocks.turncodr.mycurriculum.services.ModuleJpaRepository;
-
-import java.util.List;
 
 /**
  * Controller for the Examination Regulations sites.
@@ -28,34 +24,19 @@ public class ExRegController {
     @GetMapping("/exreg/create")
     public String getExRegCreate(Model model) {
 
-        //find modules that aren't mapped (ExReg == null)
-        List<Module> modules = moduleJpaRepository.findByExReg(null);
-
-        //add ExRegWrapper as Command Object for the form values
-        ExRegWrapper exRegWrapper = new ExRegWrapper();
-        exRegWrapper.setModules(modules);
-        exRegWrapper.setExReg(new ExReg());
-        model.addAttribute("exRegWrapper", exRegWrapper);
+        //add ExReg as Command Object for the form values
+        model.addAttribute("exReg", new ExReg());
 
         return "exregCreate";
     }
 
     @PostMapping("/exreg/create")
-    public String postExRegCreate(@ModelAttribute ExRegWrapper exRegWrapper, Model model) {
+    public String postExRegCreate(@ModelAttribute ExReg exReg, Model model) {
 
         //save the newly created ExReg
-        ExReg savedExReg = exRegJpaRepository.save(exRegWrapper.getExReg());
+        ExReg savedExReg = exRegJpaRepository.save(exReg);
 
-        //map the Modules that have been selected to the ExReg
-        List<Module> selectedModules = exRegWrapper.getSelectedModules();
-        for (Module selectedModule : selectedModules) {
-            selectedModule.setExReg(savedExReg);
-        }
-
-        //update the Module entities
-        moduleJpaRepository.saveAll(selectedModules);
-
-        return "redirect:/exreg/showList";
+        return "redirect:/exreg/list";
     }
 
 }
