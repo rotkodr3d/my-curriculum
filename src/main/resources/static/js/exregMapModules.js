2c724f25fb4f1f191f4581f462e0153264d6f079
Bastian Schattenberg <git@bestyan.de>
2018-11-17 01:14:03 +0100

    reworked exregMapModules.js with jquery for better readability (no changes in logic) and improved exregMapModules.html with bootstrap

--- src/main/resources/static/js/exregMapModules.js
+++ src/main/resources/static/js/exregMapModules.js
@@ -1,72 +1,84 @@
-$(document).ready(function() {
-	//TODO
+$(document).ready(function () {
+    //TODO
 });
 
 var semesterCounter = 0;
 var listOfUnmappedModules = [];
 
+/**
+ * Creates a new semester panel and adds it to the list.
+ */
 function addNewSemester() {
-	var semesterContainer = document.getElementById("semesterContainer");
-	var semester = document.createElement("div");
-	var semesterTitle = document.createElement("div");
-	var semesterBody = document.createElement("div"); //Body of semester which contains the modules 
-	semester.draggable = "true";
-	semesterCounter++;
-	semesterTitle.className="panel-heading";
-	semesterTitle.innerText = semesterCounter + ". Semester";
-	semesterBody.id = "semesterbody" + semesterCounter;
-	semesterBody.className = "semesterBody";
-	semesterBody.ondragover = function(event) {
-		event.preventDefault();
-	};
-	semesterBody.ondrop = function(event) {
-		event.preventDefault();
-		var data = event.dataTransfer.getData("text");
-		var element = document.getElementById(data);
-		alert(data);
-		event.target.appendChild(element);
-	} ;
-	semester.appendChild(semesterTitle);
-	semester.appendChild(semesterBody);
-	semesterContainer.appendChild(semester);
+    semesterCounter++;
+
+    var semester = $('<div></div>', {
+        draggable: true,
+        "class": "card"
+    });
+
+    var semesterBody = $('<div></div>', {
+        id: "semesterbody" + semesterCounter,
+        "class": "card-body card-deck",
+        on: {
+            dragover: function (event) {
+                event.preventDefault();
+            },
+            drop: function (event) {
+                event.preventDefault();
+                //wonky hack because jQuery only passes the jQuery event object instead of the browser event object
+                event.dataTransfer = event.originalEvent.dataTransfer;
+                var draggedElementId = event.dataTransfer.getData("text");
+                $('#' + draggedElementId).detach().appendTo(this);
+            }
+        }
+    });
+
+    var semesterTitle = $('<div></div>', {
+        "class": "card-header",
+        text: semesterCounter + ". Semester",
+        on: {
+            dragover: function (event) {
+                event.preventDefault();
+            },
+            drop: function (event) {
+                event.preventDefault();
+                //wonky hack because jQuery only passes the jQuery event object instead of the browser event object
+                event.dataTransfer = event.originalEvent.dataTransfer;
+                var draggedElementId = event.dataTransfer.getData("text");
+                $('#' + draggedElementId).detach().appendTo(semesterBody);
+            }
+        }
+    });
+
+    semester.append(semesterTitle);
+    semester.append(semesterBody);
+    $("#semesterContainer").append(semester);
 }
 
 function allowDrag(event) {
-	event.preventDefault();
+    event.preventDefault();
 }
 
 function moduleListDrop(event) {
-	event.preventDefault();
-	var data = event.dataTransfer.getData("text");
-	var element = document.getElementById(data);
-	event.target.appendChild(element);
+    event.preventDefault();
+    var moduleId = event.dataTransfer.getData("text");
+    $('#' + moduleId).detach().appendTo($('#unmappedModulesList'));
 }
 
-function dragModuleEnd(element) {
-	element.style.opacity = 1.0;	
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
 }
 
-function dragModule(event,element) {
-	event.dataTransfer.setData("text", event.target.id);
-	element.style.opacity = 0.4;
-}
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    var element = document.getElementById(data);
 
-function drag(ev) {
-	ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-	ev.preventDefault();
-	var data = ev.dataTransfer.getData("text");
-	var element = document.getElementById(data);
-
-	//element.style.opacity = 1.0;
-
-	if (ev.target === document.getElementById("div1")) {
-		ev.target.appendChild(element);
-	} else if (ev.target === document.getElementById("list")) {
-		ev.target.appendChild(element);
-	} else {
-		return;
-	}
+    if (event.target === document.getElementById("div1")) {
+        event.target.appendChild(element);
+    } else if (event.target === document.getElementById("list")) {
+        event.target.appendChild(element);
+    } else {
+        return;
+    }
 }
