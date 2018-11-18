399ff86feaab0e82b6f8a3a7c1909977fa3e6acb
Bastian Schattenberg <git@bestyan.de>
2018-11-19 00:35:00 +0100

    fixed indentation (spaces instead of tabs)

--- src/main/resources/static/js/exregMapModules.js
+++ src/main/resources/static/js/exregMapModules.js
@@ -5,27 +5,27 @@ $(document).ready(function () {
 var semesterCounter = 1;
 var listOfUnmappedModules = [];
 var delBtnSemesterId = {};//object which maps the delete button ids to semester id
-var listOfSemester = {};//object which maps the semester id to the semesterCounter 
+var listOfSemester = {};//object which maps the semester id to the semesterCounter
 
 /**
  * Creates a new semester panel and adds it to the list.
  */
 function addNewSemester() {
-	if (semesterCounter < 1) {
-		semesterCounter = 1;
-	}
+    if (semesterCounter < 1) {
+        semesterCounter = 1;
+    }
     var semesterId = "semester" + semesterCounter;
     var delBtnId = "deleteButton" + semesterCounter;
     var spanId = "semesterSpan" + semesterCounter;
     var semesterBodyId = "semesterbody" + semesterCounter;
     delBtnSemesterId[delBtnId] = semesterId;
     listOfSemester[semesterId] = {
-    		"semesterNr": semesterCounter,
-    		"span": spanId,
-    		"delBtn": delBtnId,
-    		"body": semesterBodyId
+        "semesterNr": semesterCounter,
+        "span": spanId,
+        "delBtn": delBtnId,
+        "body": semesterBodyId
     };
-    
+
     var semester = $('<div></div>', {
         draggable: true,
         "class": "card",
@@ -53,7 +53,7 @@ function addNewSemester() {
 
     var semesterTitle = $('<div></div>', {
         "class": "card-header",
-        html: "<span id="+spanId+">" + semesterCounter + ". Semester</span>",
+        html: "<span id=" + spanId + ">" + semesterCounter + ". Semester</span>",
         on: {
             dragover: function (event) {
                 event.preventDefault();
@@ -72,37 +72,37 @@ function addNewSemester() {
         "class": "btn btn-danger",
         "id": delBtnId,
         on: {
-        	click: function(obj) { //delete function with bugs, fixing needed
-        		var idToDelete = delBtnSemesterId[obj.target.id];
-        		var semesterToDelete = listOfSemester[idToDelete];       		
-        		var lastSemester = $("#semesterContainer").children(":last");	
-        		var modules = {};
-        		var modulesLast = null;
-        		
-        		for (var i = semesterCounter-1; i >= semesterToDelete.semesterNr; i--) {
-        			var nextSemester = 1;
-        			var idThis = "#semester"+i;
-        			
-        			modules = $(idThis).find("div.module");
-        			if (i > semesterToDelete.semesterNr) {
-	        			if (modulesLast == null) {	
-	    					$(idThis).find("div.modulesArea").empty();
-	    					modulesLast = modules;
-	        			} else {
-	        				$(idThis).find("div.modulesArea").empty();
-	        				$(idThis).find("div.modulesArea").append(modulesLast);
-	        				moduleLast = modules;
-	        			}
-        			}
-        			else if (i == semesterToDelete.semesterNr) {
-        				$("#unmappedModulesList").append(modules);
-        				$(idThis).find("div.modulesArea").append(modulesLast);
-        			}
-        		}
-        		
-        		lastSemester.remove();
-        		semesterCounter--;
-        	}
+            click: function (obj) { //delete function with bugs, fixing needed
+                var idToDelete = delBtnSemesterId[obj.target.id];
+                var semesterToDelete = listOfSemester[idToDelete];
+                var lastSemester = $("#semesterContainer").children(":last");
+                var modules = {};
+                var modulesLast = null;
+
+                for (var i = semesterCounter - 1; i >= semesterToDelete.semesterNr; i--) {
+                    var nextSemester = 1;
+                    var idThis = "#semester" + i;
+
+                    modules = $(idThis).find("div.module");
+                    if (i > semesterToDelete.semesterNr) {
+                        if (modulesLast == null) {
+                            $(idThis).find("div.modulesArea").empty();
+                            modulesLast = modules;
+                        } else {
+                            $(idThis).find("div.modulesArea").empty();
+                            $(idThis).find("div.modulesArea").append(modulesLast);
+                            moduleLast = modules;
+                        }
+                    }
+                    else if (i == semesterToDelete.semesterNr) {
+                        $("#unmappedModulesList").append(modules);
+                        $(idThis).find("div.modulesArea").append(modulesLast);
+                    }
+                }
+
+                lastSemester.remove();
+                semesterCounter--;
+            }
         }
     }).append($('<i></i>', {
             "class": "fa fa-trash"
