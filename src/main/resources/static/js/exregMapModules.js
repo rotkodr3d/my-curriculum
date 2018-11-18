af9dec1b0fd19bec279d9fd7ec6a729ac4261647
Christian Kotzur <derdoktor48@web.de>
2018-11-18 11:47:22 +0100

    added delete button function (with some bugs :P)

--- src/main/resources/static/js/exregMapModules.js
+++ src/main/resources/static/js/exregMapModules.js
@@ -2,23 +2,39 @@ $(document).ready(function () {
     //TODO
 });
 
-var semesterCounter = 0;
+var semesterCounter = 1;
 var listOfUnmappedModules = [];
+var delBtnSemesterId = {};//object which maps the delete button ids to semester id
+var listOfSemester = {};//object which maps the semester id to the semesterCounter 
 
 /**
  * Creates a new semester panel and adds it to the list.
  */
 function addNewSemester() {
-    semesterCounter++;
-
+	if (semesterCounter < 1) {
+		semesterCounter = 1;
+	}
+    var semesterId = "semester" + semesterCounter;
+    var delBtnId = "deleteButton" + semesterCounter;
+    var spanId = "semesterSpan" + semesterCounter;
+    var semesterBodyId = "semesterbody" + semesterCounter;
+    delBtnSemesterId[delBtnId] = semesterId;
+    listOfSemester[semesterId] = {
+    		"semesterNr": semesterCounter,
+    		"span": spanId,
+    		"delBtn": delBtnId,
+    		"body": semesterBodyId
+    };
+    
     var semester = $('<div></div>', {
         draggable: true,
-        "class": "card"
+        "class": "card",
+        "id": semesterId
     });
 
     var semesterBody = $('<div></div>', {
-        id: "semesterbody" + semesterCounter,
-        "class": "card-body card-deck",
+        id: semesterBodyId,
+        "class": "card-body card-deck modulesArea",
         on: {
             dragover: function (event) {
                 event.preventDefault();
@@ -29,13 +45,15 @@ function addNewSemester() {
                 event.dataTransfer = event.originalEvent.dataTransfer;
                 var draggedElementId = event.dataTransfer.getData("text");
                 $('#' + draggedElementId).detach().appendTo(this);
+            },
+            drag: function (event) { //child has the drag function because parent has no clickable area
             }
         }
     });
 
     var semesterTitle = $('<div></div>', {
         "class": "card-header",
-        html: "<span>" + semesterCounter + ". Semester</span>",
+        html: "<span id="+spanId+">" + semesterCounter + ". Semester</span>",
         on: {
             dragover: function (event) {
                 event.preventDefault();
@@ -51,7 +69,41 @@ function addNewSemester() {
     });
 
     $('<button></button>', {
-        "class": "btn btn-danger"
+        "class": "btn btn-danger",
+        "id": delBtnId,
+        on: {
+        	click: function(obj) { //delete function with bugs, fixing needed
+        		var idToDelete = delBtnSemesterId[obj.target.id];
+        		var semesterToDelete = listOfSemester[idToDelete];       		
+        		var lastSemester = $("#semesterContainer").children(":last");	
+        		var modules = {};
+        		var modulesLast = null;
+        		
+        		for (var i = semesterCounter-1; i >= semesterToDelete.semesterNr; i--) {
+        			var nextSemester = 1;
+        			var idThis = "#semester"+i;
+        			
+        			modules = $(idThis).find("div.module");
+        			if (i > semesterToDelete.semesterNr) {
+	        			if (modulesLast == null) {	
+	    					$(idThis).find("div.modulesArea").empty();
+	    					modulesLast = modules;
+	        			} else {
+	        				$(idThis).find("div.modulesArea").empty();
+	        				$(idThis).find("div.modulesArea").append(modulesLast);
+	        				moduleLast = modules;
+	        			}
+        			}
+        			else if (i == semesterToDelete.semesterNr) {
+        				$("#unmappedModulesList").append(modules);
+        				$(idThis).find("div.modulesArea").append(modulesLast);
+        			}
+        		}
+        		
+        		lastSemester.remove();
+        		semesterCounter--;
+        	}
+        }
     }).append($('<i></i>', {
             "class": "fa fa-trash"
         })
@@ -60,6 +112,7 @@ function addNewSemester() {
     semester.append(semesterTitle);
     semester.append(semesterBody);
     $("#semesterContainer").append(semester);
+    semesterCounter++;
 }
 
 function deleteSemester() {
