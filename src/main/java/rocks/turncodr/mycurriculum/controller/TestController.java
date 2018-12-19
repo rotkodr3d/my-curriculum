212be2f2b7d5c98d97e29701cc2bff2728a1e832
rotkodr3d <derdoktor48@web.de>
2018-12-19 15:48:35 +0100

    pdf generation works

--- src/main/java/rocks/turncodr/mycurriculum/controller/TestController.java
+++ src/main/java/rocks/turncodr/mycurriculum/controller/TestController.java
@@ -7,9 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import rocks.turncodr.mycurriculum.services.PdfGeneratorUtil;
 
 @Controller
@@ -18,12 +22,11 @@ public class TestController {
     @Autowired
     PdfGeneratorUtil pdfGeneratorUtil;
     
-	@GetMapping("/test")
-	public String getTest(HttpServletRequest request, HttpServletResponse response, Model model) throws Exception {
+	@RequestMapping(value = "/download" , method = RequestMethod.GET , produces = "application/pdf")
+	public ResponseEntity<InputStreamResource> getTest(HttpServletRequest request, HttpServletResponse response, Model model) throws Exception {
 		Map<String,String> data = new HashMap<String,String>();
 		data.put("name", "Chris");
 		model.addAttribute("name", "Chris");
-		pdfGeneratorUtil.createPdf("test", data, request, response);
-		return "test";
+		return pdfGeneratorUtil.createPdfDownload("test", data, request, response);
 	}
 }
--- src/main/java/rocks/turncodr/mycurriculum/services/PdfGeneratorUtil.java
+++ src/main/java/rocks/turncodr/mycurriculum/services/PdfGeneratorUtil.java
@@ -1,16 +1,17 @@
 package rocks.turncodr.mycurriculum.services;
 
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.UUID;
-
+import java.util.Map.Entry;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 import org.thymeleaf.TemplateEngine;
@@ -21,30 +22,32 @@ import org.xhtmlrenderer.pdf.ITextRenderer;
 public class PdfGeneratorUtil {
     @Autowired
     private TemplateEngine templateEngine;
-    public void createPdf(String templateName, Map map, HttpServletRequest request, HttpServletResponse response) throws Exception {
+      
+    public ResponseEntity<InputStreamResource> createPdfDownload(String templateName, Map<?,?> map, HttpServletRequest request, HttpServletResponse response) throws Exception {
         Assert.notNull(templateName, "The templateName can not be null");
         WebContext context = new WebContext(request, response, request.getServletContext(), request.getLocale());
         if (map != null) {
-             Iterator itMap = map.entrySet().iterator();
-               while (itMap.hasNext()) {
-              Map.Entry pair = (Map.Entry) itMap.next();
-                  context.setVariable(pair.getKey().toString(), pair.getValue());
-            }
+            for (Entry<?,?> entry : map.entrySet()) {
+                context.setVariable(entry.getKey().toString(), entry.getValue());
+            } 
         }
         
           String processedHtml = templateEngine.process(templateName, context);
-          FileOutputStream os = null;
-          String fileName = UUID.randomUUID().toString();
+          ByteArrayOutputStream os = null;
             try {
-                final File outputFile = File.createTempFile(fileName, ".pdf");
-                os = new FileOutputStream(outputFile);
+                os = new ByteArrayOutputStream();
 
                 ITextRenderer renderer = new ITextRenderer();
                 renderer.setDocumentFromString(processedHtml);
                 renderer.layout();
                 renderer.createPDF(os, false);
                 renderer.finishPDF();
-                System.out.println("PDF created successfully");
+                
+                System.out.println("PDF created successfully, returning...");
+                return ResponseEntity.ok()
+                		.contentLength(os.size())
+                		.contentType(MediaType.parseMediaType("application/pdf"))
+                		.body(new InputStreamResource(new ByteArrayInputStream(os.toByteArray())));
             }
             finally {
                 if (os != null) {
