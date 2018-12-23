d19ad49d0103d00523e6d70729601152cbb36fc6
rotkodr3d <derdoktor48@web.de>
2018-12-23 16:53:58 +0100

    Added modal for module overview

--- src/main/resources/static/js/moduleOverview.js
+++ src/main/resources/static/js/moduleOverview.js
@@ -1,15 +1,23 @@
 /**
  * 
  */
-
 function showModuleDetails (event) {
-    console.log(event);
-    var clickedModuleId = (event.target.id == "") ? $(event.target).parents('.module').attr('id') : event.target.id;
-    console.log(clickedModuleId);
+    var id = (event.target.id == "") ? $(event.target).parents(".module").attr("id") : event.target.id;
     
-    exRegModules.forEach(function (module) {
-        if (module.id == clickedModuleId) {
-            console.log(module);
-        }
+    $.ajax({
+        url: "/details/"+id,
+        success: function(response) {
+            if ($("#showModuleDetails").length) {
+                $("#showModuleDetails").remove();
+            }
+                $("body").append(response);
+                $("#showModuleDetails").modal();
+                $("#showModuleDetails").modal("show");
+            }
     });
 }
+
+/*
+ * function closeModal(event) { $("#showModuleDetails").modal("hide");
+ * $('body').removeClass('modal-open'); $('.modal-backdrop').remove(); }
+ */
