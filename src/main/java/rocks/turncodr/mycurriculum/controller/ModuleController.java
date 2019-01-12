2287eceaead690ce71c38a4f9ed86bd357408951
rotkodr3d <derdoktor48@web.de>
2019-01-12 15:18:00 +0100

    added module edit functionality

--- src/main/java/rocks/turncodr/mycurriculum/controller/ModuleController.java
+++ src/main/java/rocks/turncodr/mycurriculum/controller/ModuleController.java
@@ -1,6 +1,8 @@
 package rocks.turncodr.mycurriculum.controller;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import rocks.turncodr.mycurriculum.model.AreaOfStudies;
 import rocks.turncodr.mycurriculum.model.Module;
@@ -36,7 +39,29 @@ public class ModuleController {
         model.addAttribute("module", module);
         List<AreaOfStudies> areaOfStudiesList = areaOfStudiesJpaRepository.findAll();
         model.addAttribute("areaOfStudies", areaOfStudiesList);
+        model.addAttribute("editMode",false);
         // Set moduleCreate.html as template to be parsed
+        return "moduleCreate";
+    }
+
+    @GetMapping("/module/edit")
+    public String getAreaOfStudiesEdit(@RequestParam(value = "id", required = false, defaultValue = "0") String urlId, Model model) {
+        Integer id;
+        try {
+            id = Integer.parseInt(urlId);
+        } catch (NumberFormatException e) {
+            id = 0;
+        }
+        Optional<Module> moduleResult = moduleJpaRepository.findById(id);
+        if (moduleResult.isPresent()) {
+            Module module = moduleResult.get();
+            model.addAttribute("module", module);
+            List<AreaOfStudies> areaOfStudiesList = areaOfStudiesJpaRepository.findAll();
+            model.addAttribute("areaOfStudies", areaOfStudiesList);
+            model.addAttribute("editMode",true);
+        } else {
+            model.addAttribute("error", "module.moduleDoesntExist");
+        }
         return "moduleCreate";
     }
 
@@ -50,6 +75,28 @@ public class ModuleController {
             return "moduleCreate";
         } else {
             // Saving the form values to database
+            moduleJpaRepository.save(module);
+            return "redirect:/module/list";
+        }
+    }
+
+    @PostMapping("/module/edit")
+    public String postModuleEdit(@RequestParam(value = "id", required = false, defaultValue = "0") String urlId,
+        Model model, @Valid @ModelAttribute Module module, BindingResult bindingResult) {
+        Integer id;
+        try {
+            id = Integer.parseInt(urlId);
+        } catch (NumberFormatException e) {
+            id = 0;
+        }
+
+        if (bindingResult.hasErrors()) {
+            // validation failed, therefore stay on the page
+            List<AreaOfStudies> areaOfStudiesList = areaOfStudiesJpaRepository.findAll();
+            model.addAttribute("areaOfStudies", areaOfStudiesList);
+            return "moduleCreate";
+        } else {
+            module.setId(id);
             moduleJpaRepository.save(module);
             return "redirect:/module/list";
         }
